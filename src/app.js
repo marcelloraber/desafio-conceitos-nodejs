a4d33de4960fe06ee1f2264cff00335355827729
Marcello Raber <marcelloraber2012@gmail.com>
2020-08-23 19:08:40 -0400

    Subindo desafio Nodejs

--- src/app.js
+++ src/app.js
@@ -1,33 +1,97 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { uuid } = require('uuidv4'); // ativa uuid
+
+//const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = []; // Armazena os dados
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const {title, url, techs = []} = request.body;
+  
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id,
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not Found' });
+  }
+
+  const likes = (repositoryIndex.likes = 0);
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+    );
+
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository not found'});
+  }
+  
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({error: 'Repository not exists'});
+  }
+
+   repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
